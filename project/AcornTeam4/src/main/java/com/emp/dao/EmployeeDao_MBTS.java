9b2b608327ec23362c22f61764a21589cd046e06
acorn <acorn@DESKTOP-8OBL85H>
2022-07-08 16:07:34 +0900

    basic folder struct

---
+++ project/AcornTeam4/src/main/java/com/emp/dao/EmployeeDao_MBTS.java
@@ -0,0 +1,23 @@
+package com.emp.dao;
+
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+
+import com.emp.dto.Employee;
+
+public class EmployeeDao_MBTS
+{
+
+	public EmployeeDao_MBTS()
+	{
+		// TODO Auto-generated constructor stub
+	}
+
+	public List<Employee> getAllEmployee(SqlSession session)
+	{
+		List<Employee> list = session.selectList("com.emp.EmployeeMapper.selectAll");
+		return list;
+	}
+
+}
